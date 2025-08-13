5727ab6e2147e2660ec1a62193c37e7567b02c25
shalini mittal <shalini06mittal@gmail.com>
2025-08-13 19:32:26 +0530

    jdbc customer management

--- CoreJavaDemo/src/database/TestDB.java
+++ CoreJavaDemo/src/database/TestDB.java
@@ -1,9 +1,21 @@
 package database;
 
+import java.util.List;
+
 public class TestDB {
 
 	public static void main(String[] args) {
-		MyConnection.connect();
+		//MyConnection.connect();
+		
+		CustomerDatabase database = new CustomerDatabase();
+		//database.insertCustomer(new Customer("Divya","divya@gmail.com","divya123"));
+		//database.insertCustomerPreparedStatement(new Customer("Shivam","shivam@gmail.com","shivam123"));
+		//System.out.println(database.getLastInsertedCustomer());
+		List<Customer> list = database.getCustomers();
+		for(Customer cust : list) {
+			System.out.println(cust);
+		}
+		
 
 	}
 
