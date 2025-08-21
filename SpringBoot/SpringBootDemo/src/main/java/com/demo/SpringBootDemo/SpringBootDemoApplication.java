996bd301cf5cfdc13bf489ca8ff4175496a8c683
shalini mittal <shalini06mittal@gmail.com>
2025-08-21 19:38:57 +0530

    spring boot annotations

--- SpringBoot/SpringBootDemo/src/main/java/com/demo/SpringBootDemo/SpringBootDemoApplication.java
+++ SpringBoot/SpringBootDemo/src/main/java/com/demo/SpringBootDemo/SpringBootDemoApplication.java
@@ -2,6 +2,11 @@ package com.demo.SpringBootDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.demo.SpringBootDemo.service.ITokenGenerator;
+import com.demo.SpringBootDemo.service.RandomTokenGenerator;
 
 // is an opinionated framework
 @SpringBootApplication
@@ -9,7 +14,22 @@ public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Spring");
+		
+		
+		ApplicationContext context = 
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		
+		//ITokenGenerator gen = new RandomTokenGenerator();
+		
+//		for(String beanName :context.getBeanDefinitionNames())
+//		{
+//			System.out.println(beanName);
+//		}
+		
+		ITokenGenerator gen = context.getBean(RandomTokenGenerator.class);
+		
+		System.out.println(gen.generateToken());
+		
 	}
 
 }
--- SpringBoot/SpringBootDemo/src/main/java/com/demo/SpringBootDemo/service/RandomTokenGenerator.java
+++ SpringBoot/SpringBootDemo/src/main/java/com/demo/SpringBootDemo/service/RandomTokenGenerator.java
@@ -4,18 +4,30 @@ package com.demo.SpringBootDemo.service;
 
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("ob")
 public class RandomTokenGenerator implements ITokenGenerator {
+
+	@Value("10")
+	private int origin; 
+	@Value("20")
+	private int bound;
 
 	public RandomTokenGenerator() {
 		System.out.println("Random token constructor");
 	}
+
+	public int getOrigin() { return origin; } 
+	public void setOrigin(int origin) { this.origin = origin; } 
+	public int getBound() { return bound; } 
+	public void setBound(int bound) { this.bound = bound; }
 	@Override
 	public String generateToken() {
 		// TODO Auto-generated method stub
-		return "Token " +new Random().nextInt(1,10);
+//		return "Token " +new Random().nextInt(1,10);
+		return "Token " +new Random().nextInt(origin, bound);
 	}
 
 }
