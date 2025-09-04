e9b7cee75991bbbc5b4304a0814885a28a67fa39
shalini mittal <shalini06mittal@gmail.com>
2025-09-04 20:05:07 +0530

    Book Controller updated for error message

---
+++ SpringBoot/SpringBootWebDemo/src/main/java/com/web/SpringBootWebDemo/constants/AppConstants.java
@@ -0,0 +1,10 @@
+package com.web.SpringBootWebDemo.constants;
+
+public class AppConstants {
+	
+	public static final String SUCCESS="success";
+	public static final String FAILURE="failure";
+	public static final String STATUS="status";
+	public static final String ERROR="error";
+
+}
--- SpringBoot/SpringBootWebDemo/src/main/java/com/web/SpringBootWebDemo/restapi/BookRestController.java
+++ SpringBoot/SpringBootWebDemo/src/main/java/com/web/SpringBootWebDemo/restapi/BookRestController.java
@@ -1,12 +1,17 @@
 package com.web.SpringBootWebDemo.restapi;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.web.SpringBootWebDemo.constants.AppConstants;
 import com.web.SpringBootWebDemo.entity.Book;
 import com.web.SpringBootWebDemo.service.BookService;
 
@@ -20,5 +25,28 @@ public class BookRestController {
 	@GetMapping(produces = "application/json")
 	public List<Book> getBooks(){ 
 		return bookService.getAllBooks();
+	}
+	
+	//http://localhost:8081/books/1
+//	@GetMapping("/{id}")
+//	public Book getBookById(@PathVariable int id){
+//		return bookService.getBookById(id);
+//	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> getBookById(@PathVariable int id){
+		Map<String, Object> map = new HashMap<>();
+		
+		try {
+			
+			Book book =	bookService.getBookById(id);
+			map.put(AppConstants.STATUS, AppConstants.SUCCESS);
+			map.put("book", book);
+			
+		}catch(RuntimeException e) {
+			map.put(AppConstants.STATUS, AppConstants.FAILURE);
+			map.put(AppConstants.ERROR	, e.getMessage());
+		}
+		return ResponseEntity.ok(map);
 	}
 }
