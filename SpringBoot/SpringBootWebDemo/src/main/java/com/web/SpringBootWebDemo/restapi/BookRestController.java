a203f8eb72a4a69185cb9f2a50f0bc7977cf00d5
shalini mittal <shalini06mittal@gmail.com>
2025-09-04 20:25:31 +0530

    Book Controller updated for error message

--- SpringBoot/SpringBootWebDemo/src/main/java/com/web/SpringBootWebDemo/restapi/BookRestController.java
+++ SpringBoot/SpringBootWebDemo/src/main/java/com/web/SpringBootWebDemo/restapi/BookRestController.java
@@ -8,12 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.web.SpringBootWebDemo.constants.AppConstants;
 import com.web.SpringBootWebDemo.entity.Book;
 import com.web.SpringBootWebDemo.service.BookService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/books")
@@ -23,8 +27,10 @@ public class BookRestController {
 	private BookService bookService;
 	
 	@GetMapping(produces = "application/json")
-	public List<Book> getBooks(){ 
-		return bookService.getAllBooks();
+	public List<Book> getBooks(@RequestParam(defaultValue ="kelly") String author){ 
+		if(author==null)  
+				return bookService.getAllBooks();
+		return bookService.getBooksByAuthor(author);
 	}
 	
 	//http://localhost:8081/books/1
@@ -49,4 +55,35 @@ public class BookRestController {
 		}
 		return ResponseEntity.ok(map);
 	}
+	
+	@PostMapping 
+	
+	public ResponseEntity<Object> addBook( Book book){ 
+		System.out.println("Book "+book); 
+		Map<String, Object> map = new HashMap<>(); 
+		try { 
+			Book newbook = bookService.addNewBook(book);
+			map.put(AppConstants.STATUS, AppConstants.SUCCESS);
+			map.put("book", newbook); 
+			return ResponseEntity.ok(map); 
+		} catch (RuntimeException e){ 
+			map.put(AppConstants.STATUS, AppConstants.FAILURE); 
+			map.put(AppConstants.ERROR	,e.getMessage()); 
+			return ResponseEntity.badRequest().body(map); 
+			}
+		}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
