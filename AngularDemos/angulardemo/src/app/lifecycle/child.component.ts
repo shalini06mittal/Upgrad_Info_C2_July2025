abda361a25f0898d753d3fddb55c759815aa1fc0
Sodel Admin <sodeladmin@sodels-MacBook-Pro.local>
2025-11-10 19:11:19 +0530

    angular lifecycle

--- AngularDemos/angulardemo/src/app/lifecycle/child.component.ts
+++ AngularDemos/angulardemo/src/app/lifecycle/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
   templateUrl: './child.component.html',
   styleUrl: './child.component.css'
 })
-export class ChildComponent {
+export class ChildComponent implements OnInit, OnChanges, DoCheck{
   @Input()
   country: string = 'uk';
   @Input()
@@ -21,16 +21,29 @@ export class ChildComponent {
 
   constructor() {
     console.log(`child constructor ${this.country}`);
+    console.log(`child constructor ${JSON.stringify(this.employee)}`);
+  }
+  ngDoCheck(): void {
+     console.log(`child do check ${this.country}`);
+    console.log(`child do check ${JSON.stringify(this.employee)}`);
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+     console.log(`child on changes ${this.country}`);
+    console.log(`child on changes ${JSON.stringify(this.employee)}`);
+    this.countrystates = 
+    this.data.filter(item => item.country === this.country)
+    console.log(this.countrystates)
   }
   i=1;
   interval:any;
   ngOnInit(): void {
     console.log(`child ng on init ${this.country}`);
-    this.interval = setInterval(()=> console.log(this.i++), 1000);
+    console.log(`child ng on init ${JSON.stringify(this.employee)}`);
+    //this.interval = setInterval(()=> console.log(this.i++), 1000);
   }
   // it is invoked only first time the component is loaded
   ngOnDestroy(): void {
     console.log(`child destroy`);
-    clearInterval(this.interval);
+    //clearInterval(this.interval);
   }
 }
